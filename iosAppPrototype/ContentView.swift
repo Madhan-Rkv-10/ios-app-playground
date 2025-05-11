ebe3eb8f9788928c7191fbe76c22446a8a193faa
Madhan-Rkv-10 <madhanrkv10@gmail.com>
2025-05-11 15:33:18 +0530

    feat: add core UI components and navigation example
    - Implemented navigation stack with state management
    - Added reusable components: Button, Text, Form, Section
    - Demonstrated two-way data binding
    - Included navigation title and dropdown picker
    - Added example state types

--- iosAppPrototype/ContentView.swift
+++ iosAppPrototype/ContentView.swift
@@ -8,42 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var  tapCount=0
+    @State private var name = ""
+    
+    
+    let students = ["Harry", "Hermione", "Ron"]
+      @State private var selectedStudent = "Harry"
+    
+    
+
     var body: some View {
         
         
+        
         NavigationStack(){
-            
-//            VStack {
-//                Image(systemName: "globe")
-//                    .imageScale(.large)
-//                    .foregroundStyle(.tint)
-//                Text("Hello, world!")
-//                
-//                
-//                
-//                
-//                Form(){
-//                    
-//                    Text("Enter YourName")
-//                    Text("Enter YourName")
-//                    Text("Enter YourName")
-//                    Section(){
-//                        
-//                        
-//                        Text("Enter YourName")
-//                        Text("Enter YourName")
-//                        Text("Enter YourName")
-//                    }
-//                  
-//                }
-//            } .padding()
 
+            
+            
+            
+            Button("Tap Count  \(tapCount)"){
+                
+                tapCount+=1
+            }
+            
            
-                
-                
-                
+            
+
                 
                 Form(){
+                    
+                    
+                    Section(){
+                        
+                        Picker("Select your student", selection: $selectedStudent) {
+                                          ForEach(students, id: \.self) {// here backslash self represents Unique element id
+                                              Text($0)
+                                          }
+                                      }
+
+                    }
+                    
+                    TextField("Enter your name", text: $name) // $ is two way binding we can write as well as read
+                    Text("Your Name is \(name)") // here without dollar we want o read only
+               
                     
                   
                     Section(){
